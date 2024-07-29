4abb052dff556d792fd90956d28c46cf44a59f18
Gui <g_santana26@yahoo.com.br>
2024-07-29 02:40:29 -0300

    :recycle: - corrigindo e melhorando todo o pacote

--- src/Traits/FilterEasy.php
+++ src/Traits/FilterEasy.php
@@ -2,52 +2,43 @@
 
 namespace Gsferro\FilterEasy\Traits;
 
+use Illuminate\Database\Eloquent\Builder;
+
 /**
- * @param $boolFilterFields
- * @param $likeFilterFields
+ * @param  array<int, string>  $likeFilterFields
+ * @param  array<int, string>  $boolFilterFields
  *
+ * @author Gsferro
  * @package Gsferro\FilterEasy
  */
 trait FilterEasy
 {
     /**
-     * Default attributes
+     * Apply filters to the query builder.
      *
-     * @return array
+     * @param  Builder  $builder  The query builder instance.
+     * @param  array  $filters  The array of filters.
+     * @return Builder The modified query builder instance.
      */
-    private function getBoolFilterFields(): array
+    public function scopeFilterEasy(Builder $builder, array $filters = []): Builder
     {
-        return $this->boolFilterFields ?? [];
-    }
-
-    private function getLikeFilterFields(): array
-    {
-        return $this->likeFilterFields ?? [];
-    }
-
-    /**
-     * Add filtering generic.
-     *
-     * @param  $builder : query builder.
-     * @param  $filters : array of filters.
-     * @return query builder.
-     */
-    public function scopeFilterEasy($builder, $filters = [])
-    {
-        if (!$filters) {
+        if (empty($filters)) {
             return $builder;
         }
-        $tableName             = $this->getTable();
-        $defaultFillableFields = $this->fillable;
-        foreach ($filters as $field => $value) {
 
-            // validation
-            if (empty($value)) {
+        foreach ($filters as $field => $value) {
+            /*
+            |---------------------------------------------------
+            |  Validation
+            |---------------------------------------------------
+            */
+            // nullable value (first for performance)
+            if (is_null($value)) {
                 continue;
             }
 
-            if (in_array($field, $this->getBoolFilterFields()) && $value != null) {
-                $builder->where($field, (bool)$value);
+            // default fields in fillable
+            if (!$this->checkFieldInFillableOrRelation($field)) {
                 continue;
             }
 
@@ -62,12 +53,14 @@ trait FilterEasy
             */
             if (strpos($field, ":start")) {
                 $field = str_replace(':start', '', $field);
-                $builder->whereDate($tableName . '.' . $field, '>=', "$value");
+                dump($field);
+                $builder->where($field, '>=', $value);
                 continue;
             }
             if (strpos($field, ":end")) {
                 $field = str_replace(':end', '', $field);
-                $builder->whereDate($tableName . '.' . $field, '<=', "$value");
+                dump($field);
+                $builder->where($field, '<=', $value);
                 continue;
             }
 
@@ -76,41 +69,118 @@ trait FilterEasy
             |  Has Relationship
             |---------------------------------------------------
             | exemple:
-            |   posts:post_id
-            |   $relName = posts
-            |   $fk = post_id
+            |   posts:title
+            |   relationName = posts
+            |   relationField = title
             */
             if (strpos($field, ":")) {
-                $relation = explode(":", $field);
-                $relName  = $relation[ 0 ];
-                $fk       = $relation[ 1 ];
+                $rel = explode(":", $field);
+                $relationName = $rel[0];
+                $relationField = $rel[1];
 
-                $builder = $builder->with($relName)
-                    ->whereHas($relName, function ($query) use ($field, $fk, $value) {
-                        if (in_array($field, $this->getLikeFilterFields())) {
-                            $query->where($fk, 'LIKE', "%$value%");
+                $builder = $builder
+                    ->with($relationName)
+                    ->whereHas($relationName, function ($query)
+                    use ($field, $relationField, $value) {
+                        if ($this->checkFieldInLikeFilterFields($field)) {
+                            $query->where($relationField, 'LIKE', "%$value%");
                         } else {
-                            $query->where($fk, $value);
+                            $query->where($relationField, $value);
                         }
                     });
 
                 continue;
             }
 
-            if (!in_array($field, $defaultFillableFields) || !$value) {
+            // bool
+            if ($this->checkFieldInBoolFilterFields($field)) {
+                $builder->where($field, (bool)$value);
                 continue;
             }
 
-            if (in_array($field, $this->getLikeFilterFields())) {
-                $builder->where($tableName . '.' . $field, 'LIKE', "%$value%");
-            } else {
-                if (is_array($value)) {
-                    $builder->whereIn($field, $value);
-                } else {
-                    $builder->where($field, $value);
-                }
+            // Like
+            if ($this->checkFieldInLikeFilterFields($field)) {
+                $builder->where($field, 'LIKE', "%$value%");
+                continue;
             }
+
+            // in
+            if (is_array($value)) {
+                $builder->whereIn($field, $value);
+                continue;
+            }
+
+            // exact
+            $builder->where($field, $value);
         }
+
+        // return query builder
         return $builder;
+    }
+
+    /**
+     * Retrieves the array of fields that are used for boolean filtering.
+     *
+     * @return array The array of fields.
+     */
+    private function getBoolFilterFields(): array
+    {
+        return $this->boolFilterFields ?? [];
+    }
+
+    /**
+     * Retrieves the array of fields that are used for LIKE filtering.
+     *
+     * @return array The array of fields.
+     */
+    private function getLikeFilterFields(): array
+    {
+        return $this->likeFilterFields ?? [];
+    }
+
+    /**
+     * Checks if a given field is either a relation or in the fillable array.
+     *
+     * @param  string  $field  The field to check.
+     * @return bool Returns true if the field is a relation or in the fillable array, false otherwise.
+     */
+    private function checkFieldInFillableOrRelation(string $field): bool
+    {
+        // remove a condição especial de :start | :end e relation
+        if (strpos($field, ":")) {
+            $field = explode(":", $field)[0];
+        }
+
+        // check if field is relation
+        if (method_exists(__CLASS__, $field)) {
+            return true;
+        }
+
+        // check if field is in fillable
+        return in_array($field, $this->getFillable());
+    }
+
+    /**
+     * Checks if a given field is in the like filter fields.
+     *
+     * @param  string  $field  The field to check.
+     * @return bool Returns true if the field is in the like filter fields, false otherwise.
+     */
+    private function checkFieldInLikeFilterFields(string $field): bool
+    {
+        // check if field is in checkFieldInLikeFilterFields
+        return in_array($field, $this->getLikeFilterFields());
+    }
+
+    /**
+     * Checks if a given field is in the bool filter fields.
+     *
+     * @param  string  $field  The field to check.
+     * @return bool Returns true if the field is in the bool filter fields, false otherwise.
+     */
+    private function checkFieldInBoolFilterFields(string $field): bool
+    {
+        // check if field is in boolFilterFields
+        return in_array($field, $this->getBoolFilterFields());
     }
 }
